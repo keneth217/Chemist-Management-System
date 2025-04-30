41b9011bb6b237a149c98caa9df04e8fb10aaf73
KENETH217 <kipyegonkeneth03@gmail.com>
2025-04-30 09:24:14 +0300

    Initialize chemist manangement system and setup

---
+++ Chemist-System/src/main/java/com/chemist/system/ChemistSystemApplication.java
@@ -0,0 +1,13 @@
+package com.chemist.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemistSystemApplication.class, args);
+	}
+
+}
