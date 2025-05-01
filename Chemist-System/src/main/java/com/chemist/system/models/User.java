72eac37789c75ad44f3b9178bb0a2492d8663b55
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 13:35:08 +0300

    Fixed all typos in multitenancy configurations, models and services

--- Chemist-System/src/main/java/com/chemist/system/models/User.java
+++ Chemist-System/src/main/java/com/chemist/system/models/User.java
@@ -1,40 +1,53 @@
 package com.chemist.system.models;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
-@Data
-@NoArgsConstructor
-@Setter
-@Getter
+
 @Entity
 @Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, unique = true)
     private String username;
+
     @Column(nullable = false)
     private String password;
+
     @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String phoneNo;
-    @Enumerated(EnumType.STRING)
-    private ERole role;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @Builder.Default
+    private Set<UserRole> roles = new HashSet<>();
+
     @Column(name = "chemist_id")
     private String chemistId;
+
     @Column(name = "chemist_name")
     private String chemistName;
+
+    @ElementCollection
+    @CollectionTable(name = "user_types", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "type")
+    @Builder.Default
     private Set<String> userTypes = new HashSet<>();
-    private Set<Role> roles = new HashSet<>();
+
     private boolean isActive;
 }
