ebfcf2394e7da51d2c1f26964b4fec204f417dce
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 13:35:47 +0300

    Fixed all typos in multitenancy configurations, models and services

---
+++ Chemist-System/src/main/java/com/chemist/system/services/MasterUserService.java
@@ -0,0 +1,11 @@
+package com.chemist.system.services;
+
+import com.chemist.system.models.MasterUser;
+
+public class MasterUserService {
+
+    public MasterUser findByPhoneNo(String username) {
+
+        return null;
+    }
+}
