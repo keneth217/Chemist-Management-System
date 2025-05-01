44cdafbf186d03db9339bcfc402f2d6dbc1d25f2
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 12:11:25 +0300

    Setup multitenancy databases

---
+++ Chemist-System/src/main/java/com/chemist/system/services/DefaultSettingsService.java
@@ -0,0 +1,7 @@
+package com.chemist.system.services;
+
+public class DefaultSettingsService {
+    public void applyDefaultSettings(String tenantId, String schoolName, String schoolEmail, String schoolCode, String schoolPhoneNo, String schoolAddress) {
+
+    }
+}
