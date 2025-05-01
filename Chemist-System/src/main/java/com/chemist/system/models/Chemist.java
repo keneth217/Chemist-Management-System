66b6b6de70b1396f30ac9ced3ae32df8ace57756
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 12:11:19 +0300

    Setup multitenancy databases

---
+++ Chemist-System/src/main/java/com/chemist/system/models/Chemist.java
@@ -0,0 +1,40 @@
+package com.chemist.system.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "chemists")
+public class Chemist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long chemistId;                   // Primary key
+
+    @Column(nullable = false, unique = true)
+    private String chemistName;
+    @Column(nullable = false, unique = true)
+    private String chemistOwnerName;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String address;
+    @Column(nullable = false, unique = true)
+    private String chemistCode;
+    @Column(nullable = false, unique = true)
+    private String phone;
+    @Column(nullable = false)
+    private String Location;
+    private boolean activated;
+
+    private  String dbUrl;
+
+    public Boolean getActivated() {
+
+        return activated;
+    }
+}
