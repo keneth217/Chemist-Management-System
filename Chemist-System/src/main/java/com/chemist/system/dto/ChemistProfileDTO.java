c470ee85f49aa42caa2ad90d45576e0048d461c9
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 12:11:30 +0300

    Setup multitenancy databases

---
+++ Chemist-System/src/main/java/com/chemist/system/dto/ChemistProfileDTO.java
@@ -0,0 +1,23 @@
+package com.chemist.system.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class ChemistProfileDTO {
+    private Long chemistId;
+    private String chemistName;
+    private String chemistCode;
+    private String phone;
+    private String Location;
+    private String address;
+    private String dbUrl;
+    private String email;
+    private String chemistOwnerName;
+
+
+
+
+}
