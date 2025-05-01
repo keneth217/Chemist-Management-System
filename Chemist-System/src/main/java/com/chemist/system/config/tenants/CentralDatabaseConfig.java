848d79a9be37ec61155e81e190e0d7b8b1589fd1
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 12:17:34 +0300

    Setup multitenancy databases and models

---
+++ Chemist-System/src/main/java/com/chemist/system/config/tenants/CentralDatabaseConfig.java
@@ -0,0 +1,49 @@
+package com.chemist.system.config.tenants;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.chemist.system.repository",
+        entityManagerFactoryRef = "centralEntityManagerFactory",
+        transactionManagerRef = "centralTransactionManager"
+)
+public class CentralDatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.central")
+    public DataSource centralDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "centralEntityManagerFactory")
+    @Primary
+    public LocalContainerEntityManagerFactoryBean centralEntityManagerFactory(
+            EntityManagerFactoryBuilder builder, @Qualifier("centralDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.chemist.system.models")
+                .persistenceUnit("central1")
+                .build();
+    }
+
+    @Bean(name = "centralTransactionManager")
+    public PlatformTransactionManager centralTransactionManager(
+            @Qualifier("centralEntityManagerFactory") EntityManagerFactory centralEntityManagerFactory) {
+        return new JpaTransactionManager(centralEntityManagerFactory);
+    }
+}
