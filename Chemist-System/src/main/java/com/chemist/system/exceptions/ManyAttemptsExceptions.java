4745834a0f41276d6fbb0b8a56f13d9d28950d35
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 12:11:45 +0300

    Setup multitenancy databases

---
+++ Chemist-System/src/main/java/com/chemist/system/exceptions/ManyAttemptsExceptions.java
@@ -0,0 +1,8 @@
+package com.chemist.system.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class ManyAttemptsExceptions extends Throwable {
+}
