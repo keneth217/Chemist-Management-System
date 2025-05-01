73e0a6fefc8150973a8ea89114d452c3a97c365f
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 12:13:37 +0300

    Setup multitenancy databases and models

---
+++ Chemist-System/src/main/java/com/chemist/system/exceptions/NoBusinessIdentifierException.java
@@ -0,0 +1,6 @@
+package com.chemist.system.exceptions;
+
+public class NoBusinessIdentifierException extends Throwable {
+    public NoBusinessIdentifierException(String noSchoolIdentifierProvided) {
+    }
+}
