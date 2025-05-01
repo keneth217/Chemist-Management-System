144f56f2097ebc5135c8a867e54c7a2ac34a40c5
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 13:35:52 +0300

    Fixed all typos in multitenancy configurations, models and services

--- Chemist-System/src/main/java/com/chemist/system/repository/tenant/RoleRepository.java
+++ Chemist-System/src/main/java/com/chemist/system/repository/tenant/RoleRepository.java
@@ -1,14 +1,12 @@
 package com.chemist.system.repository.tenant;
 
-import com.chemist.system.models.ERole;
-import com.chemist.system.models.Role;
+
+import com.chemist.system.models.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<UserRole, Long> {
 
-    void findByRoleName(String roleName);
-
-    Optional<Role> findByName(ERole eRole);
+    Optional<UserRole> findByName(String name);
 }
