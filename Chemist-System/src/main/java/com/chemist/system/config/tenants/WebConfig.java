4977838dc956086566df5150e08fec2ae9dc0cc0
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 12:16:45 +0300

    Setup multitenancy databases and models

---
+++ Chemist-System/src/main/java/com/chemist/system/config/tenants/WebConfig.java
@@ -0,0 +1,26 @@
+package com.chemist.system.config.tenants;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ChemistInterceptor schoolInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "X-Tenant", "Accept", "Authorization");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(schoolInterceptor);
+    }
+}
