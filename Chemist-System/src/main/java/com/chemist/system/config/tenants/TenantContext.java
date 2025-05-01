59aaacf95fc286e820d63ee8bba4219a977a5467
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 12:14:57 +0300

    Setup multitenancy databases and models

---
+++ Chemist-System/src/main/java/com/chemist/system/config/tenants/TenantContext.java
@@ -0,0 +1,16 @@
+package com.chemist.system.config.tenants;
+public class TenantContext {
+    private static final ThreadLocal<String> currentTenant = new ThreadLocal<>();
+
+    public static void setCurrentTenant(String tenantId) {
+        currentTenant.set(tenantId);
+    }
+
+    public static String getCurrentTenant() {
+        return currentTenant.get();
+    }
+
+    public static void clear() {
+        currentTenant.remove();
+    }
+}
