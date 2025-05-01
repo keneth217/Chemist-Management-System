d2146c48b10ab824a267c0043d29bca9ef51983d
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-01 13:35:15 +0300

    Fixed all typos in multitenancy configurations, models and services

--- Chemist-System/src/main/java/com/chemist/system/services/DefaultSettingsService.java
+++ Chemist-System/src/main/java/com/chemist/system/services/DefaultSettingsService.java
@@ -1,5 +1,8 @@
 package com.chemist.system.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DefaultSettingsService {
     public void applyDefaultSettings(String tenantId, String schoolName, String schoolEmail, String schoolCode, String schoolPhoneNo, String schoolAddress) {
 
