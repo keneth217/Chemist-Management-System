f19384b8607c43df0c3bbd8110c53b430533d0c9
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-02 09:00:57 +0300

    Add master user authentication and fixed auth related typos and services

--- Chemist-System/src/main/java/com/chemist/system/exceptions/NoBusinessIdentifierException.java
+++ Chemist-System/src/main/java/com/chemist/system/exceptions/NoBusinessIdentifierException.java
@@ -1,6 +1,11 @@
 package com.chemist.system.exceptions;
 
-public class NoBusinessIdentifierException extends Throwable {
-    public NoBusinessIdentifierException(String noSchoolIdentifierProvided) {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class NoBusinessIdentifierException extends RuntimeException {
+    public NoBusinessIdentifierException(String message) {
+        super(message);
     }
 }
